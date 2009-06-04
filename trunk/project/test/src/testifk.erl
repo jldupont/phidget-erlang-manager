ce2037a8914e5cb845a2f8d9b7f21d7b99361c0e
jeanlou.dupont <jeanlou.dupont@a06bc980-2b8a-11de-bfb9-e98281094dd5>
2009-06-04 20:58:26 +0000

    working!

---
+++ trunk/project/test/src/testifk.erl
@@ -0,0 +1,50 @@
+%% Author: Jean-Lou Dupont
+%% Created: 2009-06-03
+-module(testifk).
+
+%%
+%% Include files
+%%
+
+%%
+%% API
+%%
+-export([start/0, start/1, stop/0]).
+
+%% internal
+-export([init/2, loop/1]).
+
+%%
+%% Local Functions
+%%
+
+start() ->
+    start("").
+
+start(Param) ->
+    spawn_link(?MODULE, init, ["/usr/local/bin/pem_drv_PhidgetInterfaceKit_debug -1", Param]).
+
+stop() ->
+    ?MODULE ! stop.
+
+init(ExtPrg, Param) ->
+    register(?MODULE, self()),
+    process_flag(trap_exit, true),
+    Port = open_port({spawn, ExtPrg++" "++Param}, [{packet, 2}, binary, exit_status]),
+    loop(Port).
+
+loop(Port) ->
+    receive
+		
+		stop ->
+			io:format("called [stop]"),
+			erlang:port_close(Port),
+			exit(normal);
+		
+		{Port, {data, Data}} ->
+			Decoded = binary_to_term(Data),
+			io:format("Message! Decoded[~p]~n", [Decoded])
+					
+    end,
+	loop(Port).
+
